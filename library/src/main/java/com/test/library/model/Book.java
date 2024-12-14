264d8dac5f900736d0b47295f48a2706aeca9be5
EV3TH <38407419+EV3THlm@users.noreply.github.com>
2024-12-13 18:53:11 -0600

    Add files via upload

---
+++ library/src/main/java/com/test/library/model/Book.java
@@ -0,0 +1,58 @@
+package com.test.library.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "book")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_book")
+    private Long id;
+
+    private String title;
+    private String author;
+    private double price;
+    private Boolean available;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Boolean getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(Boolean available) {
+        this.available = available;
+    }
+}
